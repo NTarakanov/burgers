9b30d6a0cdd35f2eb6672886d981cbc4ea9ccc06
NTarakanov <60585592+NTarakanov@users.noreply.github.com>
2020-04-10 09:42:17 +1200

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,12 +1,38 @@
-(function () {
-  let myMap;
+let myMap;
 
-  const init = () => {
-    myMap = new ymaps.Map("map", {
-      center: [53.051717, 158.661847],
-      zoom: 7,
-    });
-  };
+const init = () => {
+  myMap = new ymaps.Map("map", {
+    center: [53.051717, 158.661847],
+    zoom: 16,
+    controls: [],
+  });
 
-  ymaps.ready(init);
-});
+  const coord = [
+    [53.051717, 158.661847],
+    [53.07874, 158.652981],
+    [53.059857, 158.625618],
+    [53.054017, 158.609098],
+  ];
+
+  const myCollection = new ymaps.GeoObjectCollection(
+    {},
+    {
+      draggable: false,
+      iconLayout: "default#image",
+      iconImageHref: "./images/icons/map-marker.svg",
+      iconImageSize: [75, 135],
+      iconImageOffset: [-35, -125],
+    }
+  );
+
+  coord.forEach((coord) => {
+    myCollection.add(new ymaps.Placemark(coord));
+  });
+
+  myMap.geoObjects.add(myCollection);
+
+  myMap.behaviors.disable("scrollZoom");
+  console.log(myMap.behaviors);
+};
+
+ymaps.ready(init);
